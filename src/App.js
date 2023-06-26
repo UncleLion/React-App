633a78cee0788949b3bdfff18ea7b43899385329
UncleLion <unclelion@ukr.net>
2023-06-26 23:35:35 +0300

    add dynamic refresh, using props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import ExtenseList from "./components/ExpenseList";
 
 function App() {
+  const expensesInfo = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div>
       <h2>Tilki ne sogodni</h2>
-      <ExtenseList />
+      <ExtenseList
+        title={expensesInfo[0].title}
+        amount={expensesInfo[0].amount}
+        date={expensesInfo[0].date}
+      />
+      <ExtenseList
+        title={expensesInfo[1].title}
+        amount={expensesInfo[1].amount}
+        date={expensesInfo[1].date}
+      />
+      <ExtenseList
+        title={expensesInfo[2].title}
+        amount={expensesInfo[2].amount}
+        date={expensesInfo[2].date}
+      />
+      <ExtenseList
+        title={expensesInfo[3].title}
+        amount={expensesInfo[3].amount}
+        date={expensesInfo[3].date}
+      />
     </div>
   );
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,17 +1,14 @@
 import "./ExpenseList.css"
 
-export default function ExtenseList() {
-    const expenseDate = new Date(2023, 8, 15 )
-    const expenseTitle = 'Car Insurance'
-    const expenceAmount = 154.61
+export default function ExtenseList(props) {
     return (
         <div className="expense-list">
             <div>
-                {expenseDate.toISOString()}
+                {props.date.toISOString()}
             </div>
             <div className="expense-list__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-list__price">${expenceAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-list__price">${props.amount}</div>
             </div>
         </div>
     )
