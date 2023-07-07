aafe70e6668e178a85194d5d84831486f69fb840
UncleLion <unclelion@ukr.net>
2023-07-07 21:06:35 +0300

    Has been replaced hardcode on dynamic in expenses array

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -15,27 +15,14 @@ export default function Expenses(props) {
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter  selected={changeFilter} onChangeFilter={filterChangeHandler} />
-      <ExpenseList
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseList
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseList
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseList
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-        />
+        <ExpensesFilter selecte={changeFilter} onChangeFilter={filterChangeHandler} />
+        {props.items.map((expense) => (
+          <ExpenseList
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
   </div>
   )
